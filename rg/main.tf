917c1e7c702de173718d8f521bb9338f34cb1dde
Harkishore Singh <harkishoresingh@gmail.com>
2025-09-09 20:35:54 +0530

    add on rg code

--- rg/main.tf
+++ rg/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "testing"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "testing1"
+  location = "West Europe"
+}
