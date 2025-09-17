1fa1c8b00e105c536aafc57bf282b55807e3869f
Harkishore Singh <harkishoresingh@gmail.com>
2025-09-17 12:23:12 +0530

    new commit

--- rg/main.tf
+++ rg/main.tf
@@ -13,17 +13,3 @@ resource "azurerm_resource_group" "rg2" {
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "rg3" {
-  name     = "testing3"
-  location = "West Europe"
-}
-
-resource "azurerm_resource_group" "rg4" {
-  name     = "testing4"
-  location = "West Europe"
-}
-
-resource "azurerm_resource_group" "rg5" {
-  name     = "testing5"
-  location = "West Europe"
-}
